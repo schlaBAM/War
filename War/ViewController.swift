b86d7c9eadb9fe8e4e2ab51088811ac33ddb9a43
schlaBAM <brett.morrison@live.com>
2017-11-08 22:00:16 -0800

    just need to add logic

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playerCard: UIImageView!
+    @IBOutlet weak var computerCard: UIImageView!
+    @IBOutlet weak var dealButton: UIButton!
+    @IBOutlet weak var playerScore: UILabel!
+    @IBOutlet weak var computerScore: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+    }
+    
 }
 
